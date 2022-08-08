333d865b3aafe02db8557eefd2a166bd8bcb5741
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-08-08 22:39:20 +0900

    ✨MantineとTailwindCSSのコンフリクト対策とTailwindCSSの影響パスしてい

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    'src/pages/**/*.{js, jsx, ts, tsx',
-    'src/components/**/*.{js, jsx, ts, tsx',
+    'src/pages/**/*.{js,ts,jsx,tsx}',
+    'src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
+  corePlugins: {
+    preflight: false,
+  },
 }
